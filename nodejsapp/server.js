23485b909acb72eaa020d46a45a77246fb5b67f9
vivekust <83585332+vivekust@users.noreply.github.com>
2021-05-24 12:18:28 +0530

    Rename firstapp.js to firstapp1.js

--- nodejsapp/server.js
+++
@@ -1,9 +0,0 @@
-
-var http =require('http');
-
-var server =http.createServer(function(req, res) {
- res.writeHead(200,{'Content-Type': 'text/plan'});
- res.end('NodeJS App');
-});
-server.listen(6060);
-console.log('Server is running at http://localhost:6060/');
